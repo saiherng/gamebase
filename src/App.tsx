d9367814820fe1f37774bf9e4b2fa454e2ad8b44
saiherng <saiherng14@gmail.com>
2025-06-07 18:10:19 -0700

    Created responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { Button } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  
-
   return (
-     <Button colorPalette="teal" variant="solid">
-        Email
-      </Button>
-  )
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`,
+
+    }}>
+      <GridItem area="nav" bg="coral">
+        nav
+      </GridItem>
+      
+      <GridItem area="aside" bg="gold" display={{ base: 'none', lg: 'block' }}>
+          aside
+      </GridItem>
+     
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
