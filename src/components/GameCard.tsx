4f0b8ed291692794338d6c9d4bbc687186fafc8e
saiherng <saiherng14@gmail.com>
2025-06-07 23:31:30 -0700

    Created game card and game grid

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,18 @@
+import { Card, Heading, Image } from '@chakra-ui/react'
+import type { Game } from '../hooks/useGames'
+
+interface Props {
+    game: Game
+}
+
+const GameCard = ({ game }: Props) => (
+  <Card.Root borderRadius={10} overflow='hidden'>
+    <Image src={game.background_image} />
+    <Card.Body>
+      <Heading fontSize='2xl'>{game.name}</Heading>
+    </Card.Body>
+  </Card.Root>
+);
+
+
+export default GameCard;
