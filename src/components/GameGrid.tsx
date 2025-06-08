7594ff99ef0ed3514f4b115bbf66b52578942cc7
saiherng <saiherng14@gmail.com>
2025-06-08 02:10:54 -0700

    Create generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,34 +1,21 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
-import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
-import GameCardContainer from "./GameCardContainer";
+import { SimpleGrid, Text } from '@chakra-ui/react'
+import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
 
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+   const {data, error, isLoading} = useGames();
 
+   const skeletons = [1,2,3,4,5,6,7,8];
+  
   return (
     <div>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        columnGap="4"
-        rowGap="4"
-        padding="30px"
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSkeleton key={skeleton} />
-            </GameCardContainer>
-          ))}
-        {games.map((game) => (
-          <GameCardContainer>
-            <GameCard key={game.id} game={game} />
-          </GameCardContainer>
-        ))}
+    {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} columnGap="4" rowGap="4" padding='30px'>
+        {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton}/></GameCardContainer> )}
+        {data.map( game => <GameCardContainer><GameCard key={game.id} game={game} /></GameCardContainer> )}
       </SimpleGrid>
     </div>
   );
